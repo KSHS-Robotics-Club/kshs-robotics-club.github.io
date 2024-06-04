08935f07a2d2ba4504c9e4e377f665a9b8ff7a5b
SPLATPLAYS <splatplaysisgood@gmail.com>
2024-06-04 20:52:31 +0800

    Removed some useless unesecary files

--- js/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const yearSpan = document.querySelector("footer p");
-    yearSpan.textContent = `© ${new Date().getFullYear()} KSHS Robotics Club`;
-});
